cab30837b439564b6520da03d13f182641821fb2
Soumen Singh <Soumensingh951@gmail.com>
2023-10-19 07:51:44 +0530

    clinicalAiAssistance

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,84 +1,84 @@
 import React from 'react'
 import "./footer.css"
-import {  FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { FaInstagram, FaLinkedin, FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className='footer'>
       <div className='left'>
         <div className='about'>
-        <h3>HealthCareAIAssistance</h3>
-            <div className='p1'>
-                @{new Date().getFullYear()} HealthCareAI.<br/> All Rights Reserved
-            </div>
+          <h3>ClinicalAIAssistance</h3>
+          <div className='p1'>
+            @{new Date().getFullYear()} ClinicalAI.<br /> All Rights Reserved
+          </div>
         </div>
         <div className='suscribe'>
-        <h3>Subscribe to get important updates</h3>
-            <form action="#">
-              <input
-                type="email"
-                required
-                autoComplete="off"
-                placeholder="Email"
-              />
-              <input type="submit" value="Subscribe" />
-            </form>
+          <h3>Subscribe to get important updates</h3>
+          <form action="#">
+            <input
+              type="email"
+              required
+              autoComplete="off"
+              placeholder="Email"
+            />
+            <input type="submit" value="Subscribe" />
+          </form>
         </div>
       </div>
       {/* right portion */}
       <div className='right'>
         {/* first child */}
-      <div className="footer-social">
-            <h3>Follows Us</h3>
-            <div className="footer-social--icons">
-              <div>
+        <div className="footer-social">
+          <h3>Follows Us</h3>
+          <div className="footer-social--icons">
+            <div>
               <a rel="noreferrer"
-                  href="https://twitter.com/_Clinical_AI_/"
-                  target="_blank">
-                  <FaTwitter className="icons" />
-                </a>
-              </div>
-              <div>
+                href="https://twitter.com/_Clinical_AI_/"
+                target="_blank">
+                <FaTwitter className="icons" />
+              </a>
+            </div>
+            <div>
               <a rel="noreferrer"
-                  href="https://www.instagram.com/"
-                  target="_blank">
-                  <FaInstagram className="icons" />
-                </a>
-              </div>
-              <div>
-                <a rel="noreferrer"
-                  href="https://www.linkedin.com/company/clinical-ai-assistance"
-                  target="_blank">
-                  <FaLinkedin className="icons"  />
-                </a>
-              </div>
+                href="https://www.instagram.com/"
+                target="_blank">
+                <FaInstagram className="icons" />
+              </a>
+            </div>
+            <div>
+              <a rel="noreferrer"
+                href="https://www.linkedin.com/company/clinical-ai-assistance"
+                target="_blank">
+                <FaLinkedin className="icons" />
+              </a>
             </div>
           </div>
-         {/* second child */}
-         <div className="footer-contact">
-            <h3>Contact Us</h3>
-            <div className='p1'>Foundation For Innovators In Science And Technology (FIIST)<br/>
-Block 9, 5th Floor, R-503/7<br/>
-Indian Institute of Technology, Patna<br/>
-Bihta, Bihar 801106 India<br/>
-Email:healthcareaiassistance@gmail.com<br/>
-Mob: +91 9123979259
-</div>
+        </div>
+        {/* second child */}
+        <div className="footer-contact">
+          <h3>Contact Us</h3>
+          <div className='p1'>Foundation For Innovators In Science And Technology (FIIST)<br />
+            Block 9, 5th Floor, R-503/7<br />
+            Indian Institute of Technology, Patna<br />
+            Bihta, Bihar 801106 India<br />
+            Email:healthcareaiassistance@gmail.com<br />
+            Mob: +91 9123979259
           </div>
-          {/* #rd child */}
-          <div className="footer-sponsor">
-            <h3>Sponsored By</h3>
-            <div className='gap'>
-              <div className="microsoft">
-                 <img src=".\images\Microsoft.jpeg" alt="" />
-              </div>
-              <div className="fist">
+        </div>
+        {/* #rd child */}
+        <div className="footer-sponsor">
+          <h3>Sponsored By</h3>
+          <div className='gap'>
+            <div className="microsoft">
+              <img src=".\images\Microsoft.jpeg" alt="" />
+            </div>
+            <div className="fist">
               <img src=".\images\fist2.png" alt="" />
-              </div>
             </div>
           </div>
         </div>
       </div>
-   
+    </div>
+
   )
 }
 
